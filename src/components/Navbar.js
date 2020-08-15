4cf2cb782f6786bb286d99c9df4172ea4b2bb5a8
Lucas de Queiroz <lucasqreis@gmail.com>
2020-08-15 16:24:25 -0300

    hour2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import logo from "../logo.svg";
+import {ButtonContainer} from './Button';
+import styled from 'styled-components';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
             {/*
             https://www.iconfinder.com/icons/1243689/call_phone_icon
             Creative Commons (Attribution 3.0 Unported);
@@ -20,13 +22,24 @@ function Navbar() {
                 </li>
             </ul>
             <Link to="/cart" className="ml-auto">
-                <button>
-                    <i className="fas fa-cart-plus" />
+                <ButtonContainer>
+                    <span className="mr-2"> 
+                        <i className="fas fa-cart-plus" />
+                    </span>
                     my cart
-                </button>
+                </ButtonContainer>
             </Link>
-        </nav>
+        </NavWrapper>
     );
 }
 
 export default Navbar;
+
+const NavWrapper = styled.nav`
+    background: var(--mainBlue);
+    .nav-link {
+        color: var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform: capitalize;
+    }
+`
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React,{Component}from 'react';
 import Product from './Product';
+import Title from './Title';
+import {storeProducts} from '../data';
+import {ProductConsumer} from '../context';
 
-function ProductList() {
-    return (
-        <div>
-            <Product />
-        </div>
-    );
+export default class ProductList extends Component {
+    state = {
+        products: storeProducts
+    };
+    render() {
+        console.log(this.state.products);
+        return (
+            <>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="our" title="products" />
+
+                        <div className="row" />
+                        <ProductConsumer>
+                           {value => {
+                               return value.products.map(product => {
+                                    return <Product key={product.id} product={product}/>;
+                               })
+                           }}
+                        </ProductConsumer>
+                    </div>
+                </div>
+                {/* <Product /> */}
+            </>
+        );
+    }    
 }
-
-export default ProductList;
